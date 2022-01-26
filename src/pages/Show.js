5df44847ad658863b05ba4ce51e43bd1dc3c7a42
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-01-26 13:06:37 -0600

    add delete to the show page/update styling

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -19,13 +19,20 @@ const Show = (props) => {
         props.updatePeople(editForm, person._id);
         // redirect people back to index
         props.history.push("/");
-    }
+    };
+
+    // removePerson for form
+    const removePerson = () => {
+        props.deletePeople(person._id);
+        props.history.push("/");
+    };
 
     return (
         <div className="person">
             <h1>{person.name}</h1>
             <h2>{person.title}</h2>
             <img src={person.image} alt={person.name} />
+            <button id="delete" onClick={removePerson}>DELETE</button>
             <form onSubmit={handleSubmit}>
                 <input type="text" value={editForm.name} name="name" placeholder="name" onChange={handleChange}/>
                 <input type="text" value={editForm.image} name="image" placeholder="image URL" onChange={handleChange}/>
