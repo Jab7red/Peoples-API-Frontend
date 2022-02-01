cde7f034c669732065a8b198b17d3d2c6397a91e
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-02-01 10:43:08 -0600

    fix refresh bug

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -2,15 +2,19 @@ import { useState } from "react";
 
 const Show = (props) => {
     const id = props.match.params.id;
-    const people = props.people;
-    const person = people.find(p => p._id === id);
+    const people = props?.people;
+    const person = people ? people.find(p => p._id === id) : null;
 
     // state for edit form
-    const [ editForm, setEditForm ] = useState(person);
+    const [editForm, setEditForm] = useState(person ? person : {
+        name: "",
+        title: "",
+        image: ""
+    });
 
     // handleChange function for form 
     const handleChange = (event) => {
-        setEditForm({...editForm, [event.target.name]: event.target.value});
+        setEditForm({ ...editForm, [event.target.name]: event.target.value });
     };
 
     // handleSubmit for form
@@ -27,16 +31,24 @@ const Show = (props) => {
         props.history.push("/");
     };
 
-    return (
-        <div className="person">
+    const loaded = () => {
+        <>
             <h1>{person.name}</h1>
             <h2>{person.title}</h2>
             <img src={person.image} alt={person.name} />
             <button id="delete" onClick={removePerson}>DELETE</button>
+        </>
+    }
+
+    const loading = () => <h1>Loading...</h1>
+
+    return (
+        <div className="person">
+            {person ? loaded() : loading()}
             <form onSubmit={handleSubmit}>
-                <input type="text" value={editForm.name} name="name" placeholder="name" onChange={handleChange}/>
-                <input type="text" value={editForm.image} name="image" placeholder="image URL" onChange={handleChange}/>
-                <input type="text" value={editForm.title} name="title" placeholder="title" onChange={handleChange}/>
+                <input type="text" value={editForm.name} name="name" placeholder="name" onChange={handleChange} />
+                <input type="text" value={editForm.image} name="image" placeholder="image URL" onChange={handleChange} />
+                <input type="text" value={editForm.title} name="title" placeholder="title" onChange={handleChange} />
                 <input type="submit" value="Update Person" />
             </form>
         </div>
